eb11240d80e933d05615d93fba2185cdac033871
bee314 <s90412test@gmail.com>
2021-12-29 16:00:46 +0800

    add shell

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,15 +2,27 @@
 #include "../cpu/timer.h"
 #include "../drivers/keyboard.h"
 #include "../drivers/screen.h"
+#include "kernel.h"
+#include "../libc/string.h"
 
 void kernel_main() {
-    clear_screen();
-
     isr_install();
-
     __asm__ volatile("sti");
-    init_timer(50);
-    /* Comment out the timer IRQ handler to read
-     * the keyboard IRQs easier */
+    /* IRQ0: timer */
+    //init_timer(50);
+    /* IRQ1: keyboard */
     init_keyboard();
+
+    kprint("Type something, it will go through the kernel\n"
+        "Type END to halt the CPU\n> ");
+}
+
+void user_input(char *input) {
+    if (strcmp(input, "END") == 0) {
+        kprint("Stopping the CPU. Bye!\n");
+        __asm__ volatile("hlt");
+    }
+    kprint("You said: ");
+    kprint(input);
+    kprint("\n> ");
 }
